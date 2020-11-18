c0177dfc815ff6676a1f236faa670204fc9a6c28
gjteamone <giljohn.ferolino@teamone.com.sg>
2020-11-18 13:23:57 +0800

    Update Project Files

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// Simple NodeJS App - Push to Heroku
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log('listening on 3000');
 })
 
 app.get('/check-server', function (req, res){
-  res.send('Hello World')
+  res.send('Your Backend Server is Ready')
 })
 
 app.post('/submit-payment', (req, res) => {
